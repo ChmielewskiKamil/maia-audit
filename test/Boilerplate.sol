5a3c826d3fee72bec66642ea1c292d540ce9ec7d
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-06-22 14:56:27 +0200

    👷 Move stuff to boilerplate

--- test/Boilerplate.sol
+++ test/Boilerplate.sol
@@ -6,6 +6,44 @@ pragma solidity ^0.8.15;
 
 import {Test, stdStorage, StdStorage, console2 as console} from "forge-std/Test.sol";
 
+import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
+import {SafeCastLib} from "solmate/utils/SafeCastLib.sol";
+import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
+import {ERC20} from "solmate/tokens/ERC20.sol";
+import {MockERC20} from "solmate/test/utils/mocks/MockERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+// Imports for minting UNI-V3 positions
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+import "@uniswap/v3-core/contracts/libraries/TickMath.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import "@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol";
+import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
+import "@uniswap/v3-periphery/contracts/base/LiquidityManagement.sol";
+
+import {UniswapV3Factory, UniswapV3Pool} from "@uniswap/v3-core/contracts/UniswapV3Factory.sol";
+
+import {IWETH9} from "@uniswap/v3-periphery/contracts/interfaces/external/IWETH9.sol";
+import {NonfungiblePositionManager} from "@uniswap/v3-periphery/contracts/NonfungiblePositionManager.sol";
+
+import {
+    UniswapV3GaugeFactory,
+    FlywheelGaugeRewards,
+    BaseV2GaugeManager
+} from "@gauges/factories/UniswapV3GaugeFactory.sol";
+import {BribesFactory, FlywheelBoosterGaugeWeight} from "@gauges/factories/BribesFactory.sol";
+import {UniswapV3Gauge, BaseV2Gauge} from "@gauges/UniswapV3Gauge.sol";
+
+import {BaseV2Minter} from "@hermes/minters/BaseV2Minter.sol";
+import {bHermes} from "@hermes/bHermes.sol";
+
+import {UniswapV3Assistant} from "@test/test-utils/UniswapV3Assistant.t.sol";
+
+import {PoolVariables} from "@talos/libraries/PoolVariables.sol";
+
+import {IUniswapV3Pool, UniswapV3Staker, IUniswapV3Staker, IncentiveTime} from "@v3-staker/UniswapV3Staker.sol";
+
 contract Boilerplate is Test {
     using stdStorage for StdStorage;
 
@@ -25,7 +63,28 @@ contract Boilerplate is Test {
 
     function initializeBoilerplate() public {
         makeAddr();
+        vm.label(address(this), "Test contract");
+        vm.createSelectFork(localhost);
     }
+
+    ////////////////////////////////////////////////////////////////////
+    //                      Addresses Arbitrum                        //
+    ////////////////////////////////////////////////////////////////////
+
+    address constant USDC = 0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8;
+    address constant WETH = 0x82aF49447D8a07e3bd95BD0d56f35241523fBab1;
+    address constant DAI = 0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1;
+
+    UniswapV3Factory uniswapV3Factory = UniswapV3Factory(0x1F98431c8aD98523631AE4a59f267346ea31F984);
+
+    INonfungiblePositionManager nonfungiblePositionManager =
+        INonfungiblePositionManager(payable(0xC36442b4a4522E871399CD717aBDD847Ab11FE88));
+
+    // UniV3 ETH/USDC 0.05% pool
+    // https://info.uniswap.org/#/arbitrum/pools/0xc31e54c7a869b9fcbecc14363cf510d1c41fa443
+    UniswapV3Pool ETH_USDC_pool = UniswapV3Pool(0xC31E54c7a869B9FcBEcc14363CF510d1c41fa443);
+    // Token0: DAI, Token1: USDC
+    UniswapV3Pool DAI_USDC_pool = UniswapV3Pool(0xF0428617433652c9dc6D1093A42AdFbF30D29f74);
 
     ////////////////////////////////////////////////////////////////////
     //                           Utilities                            //
